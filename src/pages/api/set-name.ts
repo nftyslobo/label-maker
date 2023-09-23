7cdff75862d5ee2f2ce85cd66199a3da6223ed19
Alex <aslobodnik@gmail.com>
2023-09-23 09:59:12 -0400

    add private api route

---
+++ src/pages/api/set-name.ts
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+const NAMESTONE_KEY = process.env.NAMESTONE;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === "POST") {
+    try {
+      const externalApiResponse = await fetch(
+        "https://namestone.xyz/api/public_v1/set-name",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: NAMESTONE_KEY,
+          },
+          body: JSON.stringify(req.body),
+        }
+      );
+
+      const externalApiData = await externalApiResponse.json();
+
+      // Respond with data received from the external API
+      res.status(externalApiResponse.status).json(externalApiData);
+    } catch (error) {
+      res.status(500).json({ name: "Internal Server Error" });
+    }
+  } else {
+    // Handle other HTTP methods or return an error
+    res.status(405).json({ name: "Method Not Allowed" });
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,36 @@ import styled, { css } from "styled-components";
 import { Container, Layout } from "@/components/templates";
 import { ConnectButton } from "@/components/ConnectButton";
 import { useAccount } from "wagmi";
+import { useState } from "react";
 
 export default function Home() {
   const { address, isConnected } = useAccount();
-  console.log(isConnected);
+  const [postData, setPostData] = useState(null);
+
+  const handleButtonClick = async () => {
+    try {
+      const response = await fetch("/api/set-name", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          domain: "_label.eth",
+          name: "frank",
+          address: "0x534631Bcf33BDb069fB20A93d2fdb9e4D4dD42CF",
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+
+      const data = await response.json();
+      setPostData(data);
+    } catch (error) {
+      console.error("There was an error!", error);
+    }
+  };
 
   return (
     <>
@@ -29,7 +55,10 @@ export default function Home() {
         {/* Main content */}
         <Container as="main" $variant="flexVerticalCenter" $width="large">
           <Heading level="1">Label Maker Pro {isConnected}</Heading>
-
+          <div>
+            <Button onClick={handleButtonClick}>Send POST Request</Button>
+            {postData && <div>{JSON.stringify(postData)}</div>}
+          </div>
           <div className="grid grid-cols-7 gap-4">
             <div className="col-span-4">
               <Input label="Eth Address" placeholder="0xA0Cf…251e" />
